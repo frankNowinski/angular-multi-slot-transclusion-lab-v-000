738ed3e6c1471a04b3bb423cb70b657cdaf611bc
frankNowinski <nowinski.frank@gmail.com>
2016-03-26 19:23:41 -0400

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,15 +1,23 @@
 function UserProfile() {
-	return {
-		transclude: true,
-		template: [
-			'<div>',
-				'<h3>User Profile</h3>',
-				'<div ng-transclude></div>',
-			'</div>'
-		].join('')
-	}
+  return {
+    transclude: {
+      name: 'h4',
+      position: 'h6',
+      description: 'p'
+    },
+    template: [
+      '<div>',
+        '<label>Name</label>',
+        '<div ng-transclude="name"></div>',
+        '<label>Position</label>',
+        '<div ng-transclude="position"></div>',
+        '<label>Description</label>',
+        '<div ng-transclude="description"></div>',
+      '</div>'
+    ].join('')
+  }
 }
 
 angular
-	.module('app')
-	.directive('userProfile', UserProfile);
+  .module('app')
+  .directive('userProfile', UserProfile);
